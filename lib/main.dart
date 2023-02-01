7796e0886b5cfb63b6d064023b8e58302f0b2db4
gustavofpsilva <gustavofpsilva1@gmail.com>
2023-02-01 16:36:12 -0300

    1

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Egg Count',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -25,7 +27,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Egg Count'),
     );
   }
 }
